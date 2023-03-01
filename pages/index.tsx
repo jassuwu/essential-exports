640d01ec1cb57a74f9ce6c62b21c9780f912e219
jassuwu <20pw14@psgtech.ac.in>
2023-03-01 22:43:13 +0530

    added components in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,27 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+// Import components
+import { Navbar, Footer } from '@/components'
+import { Landing, About, Products } from '@/components/index/index'
+
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1> 
+      <Head>
+        <title>Essential Exports</title>
+        <meta name="description" content="Essential Exports" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Navbar />
+        <Landing />
+        <About />
+        <Products />
+        <Footer />
+      </div>
     </>
   )
 }
