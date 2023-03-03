e793cb7beb5f1297beaf69f7e932c967df3dfad6
en3rypt <sanjithnathan@gmail.com>
2023-03-03 23:24:55 +0530

    curly lines added

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 
 const About = () => {
   return (
-    <section id='About'>
+    <section id='About' className='relative'>
 
-      {/* <Image src='/images/vector_top.svg' alt='Vector' width={1410} height={900}
-        className='absolute top-16 right-0 w-[70%] invisible md:visible ' /> */}
+      <Image src='/images/vector_top.svg' alt='Vector' width={1410} height={900}
+        className='absolute  right-0 w-[70%] -top-0 sm:-top-12  md:-top-22 xl:-top-32' />
       <div className='px-5  md:container md:mx-auto py-20 relative'>
         <h2 className=' text-2xl md:text-4xl lg:text-5xl font-anybody'>
           Who Are We?
@@ -30,8 +30,8 @@ const About = () => {
           </div>
 
           <div>
-            <div style={{ background: "linear-gradient(189.49deg, #EFFFFE 0%, #A7B0B0 20%, rgba(0, 0, 0, 0)90%)" }}
-              className='w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] lg:w-[150px] lg:h-[150px]  xl:w-[200px] xl:h-[200px]  rounded-tl-[50%] rounded-tr-[50%] rounded-bl-[50%] rounded-br-[50%] blur-[5px] sm:blur-[10px] absolute right-0 -bottom-28 sm:-bottom-28  sm:right-20 lg:right-22 xl:right-72'>
+            <div style={{ background: "linear-gradient(189.49deg, #EFFFFE 0%, #A7B0B0 20%, rgba(0, 0, 0, 0)75%)" }}
+              className='w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] lg:w-[150px] lg:h-[150px]  xl:w-[200px] xl:h-[200px]  rounded-tl-[50%] rounded-tr-[50%] rounded-bl-[50%] rounded-br-[50%] blur-[5px] sm:blur-[20px] absolute right-0 -bottom-28 sm:-bottom-28  sm:right-20 lg:right-22 xl:right-72'>
             </div>
             <h2 className='text-base sm:text-2xl md:text-2xl lg:text-4xl xl:text-5xl font-anybody rotate-90 absolute -bottom-28 right-1 sm:-bottom-32 sm:right-16 xl:right-60'>
               OUR VISION
@@ -46,9 +46,9 @@ const About = () => {
             <p className='text-sm sm:text-base md:text-lg  lg:text-2xl font-inter font-extralight max-w-2xl ml-0 md:ml-0 lg:ml-16 mt-3 md:mt-10 pl-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris in aliquam sem fringilla ut morbi tincidunt augue. </p>
           </div>
         </div>
-        {/* <Image src='/images/vector_bottom.svg' alt='Vector' width={600} height={900}
-          className='absolute -bottom-10 left-0' /> */}
       </div>
+      {/* <Image src='/images/vector_bottom.svg' alt='Vector' width={600} height={900}
+        className='absolute -bottom-10 left-0 hidden lg:block' /> */}
 
     </section>
   )
