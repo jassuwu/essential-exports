bff294ad934c87d80b135d36cffa97cdfec2e27a
JERSHA2712 <76258267+JERSHA20PW13@users.noreply.github.com>
2023-03-04 01:31:46 +0530

    mobile view almost done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -7,28 +6,28 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <div className='bg-footer-dark mt-16 h-80'>
-      <div className='flex justify-evenly mx-auto'>
-        <div className='mt-8'>
-          <Image src='/images/logo_white.png' alt='logo' width={50} height={50}></Image>
+    <div className='bg-footer-dark mt-16'>
+      <div className='md:flex-row flex flex-col justify-evenly mx-auto text-base md:text-xl'>
+        <div className='mt-8 mx-auto'>
+          <Image src='/images/logo_white.png' alt='logo' width={50} height={50} className='w-8 h-7'></Image>
         </div>
-        <div className='mt-8 font-inter'>
-          <h4 className='text-xl mb-3'>Overview</h4>
+        <div className='mt-8 font-inter text-center mx-auto'>
+          <h4 className='md:text-xl mb-3 font-bold'>Overview</h4>
           <p>Home</p>
           <p>Culture</p>
           <p>Products</p>
           <p>Enquiry</p>
           <p>Contact</p>
         </div>
-        <div className='mt-8 font-inter'>
-            <h4 className='text-xl mb-3'>Responsibility</h4>
+        <div className='mt-8 font-inter text-center mx-auto'>
+            <h4 className='md:text-xl mb-3 font-bold'>Responsibility</h4>
             <p>Community</p>
             <p>Ethical Sourcing</p>
             <p>Environment</p>
             <p>Diversity</p>
         </div>
-        <div className='mt-8 font-inter'>
-          <h4 className='text-xl mb-3'>SOCIAL MEDIA</h4>
+        <div className='mt-8 font-inter md:w-40 text-center w-60 mx-auto'>
+          <h4 className='md:text-xl mb-3 text-center font-bold'>Social Media</h4>
           <div className='flex justify-evenly'>
             <a href="#" className='inline-block w-10 h-10'><FontAwesomeIcon icon={faInstagram} /></a>
             <a href="#" className='inline-block w-6 h-6'><FontAwesomeIcon icon={faFacebookF} /></a>
@@ -36,7 +35,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className='flex justify-center mt-20'>
+      <div className='p-6 flex justify-center'>
         <hr className='w-4/5'></hr>
       </div>
     </div>
--- components/Home/Slideshow.tsx
+++ components/Home/Slideshow.tsx
@@ -20,12 +20,12 @@ const props = {
     infinite: true,
     pauseOnHover: false,
     prevArrow: (
-        <div className='ml-8'>
+        <div className='ml-5'>
             <Image src='/images/left.svg' alt='left' width={10} height={10}></Image>
         </div>
     ),
     nextArrow: (
-        <div className='mr-8'>
+        <div className='mr-5'>
             <Image src='/images/right.svg' alt='left' width={10} height={10}></Image>
         </div>
     )
@@ -37,27 +37,29 @@ const Slideshow = () => {
   
   for (let i = 0; i < len; i++) {
     let slideImages = []
+
     slideImages[0] = images[i];
     slideImages[1] = images[(i+1)%len];
     slideImages[2] = images[(i+2)%len];
-    slideImages[3] = images[(i+3)%len];
+    // slideImages[3] = images[(i+3)%len];
 
     slides.push(
-      <div className="flex justify-center" key={i}>
+      <div className="flex justify-evenly items-center" key={i} id="slides">
         {slideImages.map((location, index) => (
-          <Image src={location} alt={`Image ${index}`} key={i + index} width={300} height={400} className='p-4'/>
+          <Image src={location} alt={`Image ${index}`} key={i + index} width={250} height={350} className='md:p-3 md:w-1/5 w-1/4 object-cover'/>
         ))}
       </div>
     );
   }
 
   return (
-    <>
+    <div className=''>
       <Fade {...props}>
         {slides}
       </Fade>
-    </>
+    </div>
   )
 }
 
 export default Slideshow
+
